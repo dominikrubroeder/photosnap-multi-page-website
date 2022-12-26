f1479760508218662677167366a483ad290f120e
Dominik <dominik.rubroeder@icloud.com>
2022-12-26 11:31:06 +0100

    Add brand color and font family

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({ children }) => {
   return (
-    <button className="bg-black px-6 py-3 text-white transition duration-300 hover:bg-[#DFDFDF] hover:text-black">
+    <button className="bg-black px-6 py-3 text-white transition duration-300 hover:bg-brand-light-grey hover:text-black">
       {children}
     </button>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-light-grey': 'hsl(0°, 0%, 87%)',
+      },
+    },
   },
   plugins: [],
 };
