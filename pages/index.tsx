cdb8792619d6d150964cd0e205c362ce19da537c
Dominik <dominik.rubroeder@icloud.com>
2022-12-26 17:08:43 +0100

    Responsive improvements for Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,12 +27,12 @@ export default function Home() {
             />
           </div>
 
-          <div className="relative flex flex-col items-center justify-center gap-8 bg-black text-white md:order-1">
+          <div className="relative flex flex-col items-center justify-center gap-8 bg-black py-20 text-white md:order-1 md:py-0">
             <div className="flex w-full gap-4">
               <div className="w-1 bg-brand"></div>
 
-              <div className="relative left-1/2 grid max-w-xs flex-1 -translate-x-1/2 gap-8">
-                <div className="grid gap-4">
+              <div className="relative left-1/2 -ml-4 grid max-w-xs flex-1 -translate-x-1/2 gap-12">
+                <div className="grid gap-2">
                   <h1 className="text-[2.5rem] font-bold uppercase">
                     Create and share your photo stories.
                   </h1>
@@ -49,7 +49,7 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="grid min-h-[40.625rem] md:grid-cols-[1fr_3fr] lg:grid-cols-[3fr_1fr]">
+        <section className="grid min-h-[40.625rem] md:grid-cols-[2fr_3fr] lg:grid-cols-[3fr_2fr]">
           <div className="relative min-h-[17.8125rem]">
             <Image
               src="/home/desktop/beautiful-stories.jpg"
@@ -59,23 +59,25 @@ export default function Home() {
             />
           </div>
 
-          <div className="flex flex-col items-center justify-center gap-8 px-8 py-4">
-            <div className="grid gap-4">
-              <h2 className="text-[2.5rem] font-bold uppercase">
-                Beautiful stories every time
-              </h2>
-              <p className="opacity-[60%]">
-                We provide design templates to ensure your stories look
-                terrific. Easily add photos, text, embed maps and media from
-                other networks. Then share your story with everyone.
-              </p>
-            </div>
+          <div className="flex flex-col items-center justify-center gap-8 px-8 py-20 md:py-0">
+            <div className="grid max-w-xs gap-12">
+              <div className="grid gap-2">
+                <h2 className="text-[2.5rem] font-bold uppercase">
+                  Beautiful stories every time
+                </h2>
+                <p className="opacity-[60%]">
+                  We provide design templates to ensure your stories look
+                  terrific. Easily add photos, text, embed maps and media from
+                  other networks. Then share your story with everyone.
+                </p>
+              </div>
 
-            <Button variant="text" text="View the stories" />
+              <Button variant="text" text="View the stories" />
+            </div>
           </div>
         </section>
 
-        <section className="grid min-h-[40.625rem] md:grid-cols-[3fr_1fr] lg:grid-cols-[1fr_3fr]">
+        <section className="grid min-h-[40.625rem] md:grid-cols-[3fr_2fr] lg:grid-cols-[2fr_3fr]">
           <div className="relative min-h-[17.8125rem] md:order-2">
             <Image
               src="/home/desktop/designed-for-everyone.jpg"
@@ -85,19 +87,21 @@ export default function Home() {
             />
           </div>
 
-          <div className="flex flex-col items-center justify-center gap-8 px-8 py-4 md:order-1">
-            <div className="grid gap-4">
-              <h2 className="text-[2.5rem] font-bold uppercase">
-                Designed for everyone
-              </h2>
-              <p className="opacity-[60%]">
-                Photosnap can help you create stories that resonate with your
-                audience. Our tool is designed for photographers of all levels,
-                brands, businesses you name it.
-              </p>
-            </div>
+          <div className="flex flex-col items-center justify-center gap-8 px-8 py-20 md:order-1 md:py-0">
+            <div className="grid max-w-xs gap-12">
+              <div className="grid gap-2">
+                <h2 className="text-[2.5rem] font-bold uppercase">
+                  Designed for everyone
+                </h2>
+                <p className="opacity-[60%]">
+                  Photosnap can help you create stories that resonate with your
+                  audience. Our tool is designed for photographers of all
+                  levels, brands, businesses you name it.
+                </p>
+              </div>
 
-            <Button variant="text" text="View the stories" />
+              <Button variant="text" text="View the stories" />
+            </div>
           </div>
         </section>
 
