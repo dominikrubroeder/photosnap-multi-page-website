7bb1c36a2777c249502fd0109e99603009e93407
Dominik <dominik.rubroeder@icloud.com>
2022-12-28 15:04:54 +0100

    Mobile header navigation structure changes

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Logo from '../svg/Logo';
 import Button from '../ui/Button';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 const Header: React.FC = () => {
   return (
@@ -10,12 +11,12 @@ const Header: React.FC = () => {
         <Logo />
 
         <div className="md:flex-1">
-          <button className="group inline-grid gap-1 transition md:hidden">
+          <button className="group mb-4 inline-grid gap-1 transition md:hidden">
             <span className="h-[0.0625rem] w-5 origin-center bg-black transition group-hover:rotate-45"></span>
             <span className="h-[0.0625rem] w-5 origin-center bg-black transition group-hover:-rotate-45"></span>
           </button>
 
-          <div className="absolute left-0 mt-4 grid w-full justify-center gap-8 md:relative md:left-auto md:mt-0 md:flex md:justify-between">
+          <div className="absolute left-0 z-50 grid w-full justify-center gap-8 bg-white md:relative md:left-auto md:mt-0 md:flex md:justify-between md:py-0">
             <ul className="grid gap-4 text-center md:flex md:flex-1 md:items-center md:justify-center md:gap-2 md:text-left">
               <li className="cursor-pointer uppercase tracking-widest transition duration-300 hover:opacity-25">
                 <Link href="/stories">Stories</Link>
